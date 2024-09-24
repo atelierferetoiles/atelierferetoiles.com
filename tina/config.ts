d33722ca944a4d78e01ad61d182bb014cd584352
Apollinaire <no-reply@apollinai.re>
2024-09-24 11:33:45 +0200

    feat: lowercase posts

--- tina/config.ts
+++ tina/config.ts
@@ -120,6 +120,19 @@ export default defineConfig({
             date: new Date().toISOString(),
           };
         },
+        ui: {
+          filename: {
+            slugify: (values) => {
+              return values.title
+                .toString()
+                .toLowerCase()
+                .trim()
+                .replace(/[^\w\s-]/g, '') // remove non-word [a-z0-9_], non-whitespace, non-hyphen characters
+                .replace(/[\s_-]+/g, '_') // swap any length of whitespace, underscore, hyphen characters with a single _
+                .replace(/^-+|-+$/g, ''); // remove leading, trailing -
+            },
+          },
+        },
         fields: [
           {
             type: 'string',
